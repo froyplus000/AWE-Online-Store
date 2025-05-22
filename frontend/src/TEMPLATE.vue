5605b05331f768a2bfcaff9cc90cde22b6553d3f
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-05-22 22:02:14 +1000

    Front End basic Setup
    - Basic project setup with vite and npm
    - Need to integrate vue-router and tailwindCSS
    - Some basic vue file structures

---
+++ frontend/src/TEMPLATE.vue
@@ -0,0 +1,6 @@
+<!-- Here you put on any Import, Data (array, variables) and Functions-->
+<script setup></script>
+<!-- HTML thing -->
+<template></template>
+<!-- Styling for this specific file -->
+<style scoped></style>
