899532f73acca0009afead5f36f9e99a678b3d10
tjmud <127167455+tjmud@users.noreply.github.com>
2025-05-31 00:16:33 +1000

    Update OrderService.java

--- BackEnd/src/main/java/com/GroupSeven/AWE_Online_Store/services/OrderService.java
+++ BackEnd/src/main/java/com/GroupSeven/AWE_Online_Store/services/OrderService.java
@@ -1,7 +1,9 @@
 package com.GroupSeven.AWE_Online_Store.services;
 
+import com.GroupSeven.AWE_Online_Store.dto.CreateOrderRequest;
 import com.GroupSeven.AWE_Online_Store.dto.OrderResponse;
 import com.GroupSeven.AWE_Online_Store.entity.Order;
+import com.GroupSeven.AWE_Online_Store.entity.User;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ public interface OrderService {
     List<OrderResponse> getAllOrders();
     OrderResponse getOrderById(String userEmail, Integer orderId);
     OrderResponse updateOrderStatus(Integer orderId, String newStatus);
+    
+    
+    Order createOrder(User user, CreateOrderRequest request);
 }
