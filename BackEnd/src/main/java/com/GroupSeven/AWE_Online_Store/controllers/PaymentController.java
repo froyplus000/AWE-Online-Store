7dd4adb3f0ff0400de2549f94a15448df165dcfc
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-05-31 21:59:46 +1000

    Fixed CORS

--- BackEnd/src/main/java/com/GroupSeven/AWE_Online_Store/controllers/PaymentController.java
+++ BackEnd/src/main/java/com/GroupSeven/AWE_Online_Store/controllers/PaymentController.java
@@ -5,11 +5,9 @@ import com.GroupSeven.AWE_Online_Store.dto.PaymentResponse;
 import com.GroupSeven.AWE_Online_Store.services.PaymentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/payments")
 @AllArgsConstructor
--- BackEnd/src/main/java/com/GroupSeven/AWE_Online_Store/controllers/UserController.java
+++ BackEnd/src/main/java/com/GroupSeven/AWE_Online_Store/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
